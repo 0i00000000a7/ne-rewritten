490fb0a8b49c64066371a3c3d36ffdc3418385d8
Smile Lee <smileleelyx@qq.com>
2026-06-29 23:52:37 +0800

    代码格式化

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL || '/ne-rewritten/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    base: process.env.BASE_URL || '/ne-rewritten/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
-})
+});
